09761cc28092b8834ef40b04afb35eea816e0684
fadeomar <fadeomar2015@gmail.com>
2023-08-12 02:06:57 +0300

    general updates

--- src/App.js
+++ src/App.js
@@ -2,48 +2,15 @@
 
 import React from "react";
 import FamilyTree from "./components/FamilyTree";
+import CurvedLine from "./components/CurvedLine";
+import BarChart from "./components/BarChart";
 import "./App.css";
 
 const App = () => {
-  // Dummy family tree data
-  const familyData = {
-    name: "John Doe",
-    gender: "male",
-    children: [
-      {
-        name: "Jane Doe",
-        gender: "female",
-        children: [
-          {
-            name: "Alice Doe",
-            gender: "female",
-            children: [],
-          },
-          {
-            name: "Bob Doe",
-            gender: "male",
-            children: [],
-          },
-        ],
-      },
-      {
-        name: "Mark Doe",
-        gender: "male",
-        children: [
-          {
-            name: "Sara Doe",
-            gender: "female",
-            children: [],
-          },
-        ],
-      },
-    ],
-  };
-
   return (
     <div className="App">
-      <h1>Family Tree Visualization</h1>
-      <FamilyTree data={familyData} />
+      <h1>Visualization Component</h1>
+      <BarChart />
     </div>
   );
 };
